ccbae460fb5730196a0c472ada5106c8395d762a
MkSistemas <MkSistemas@host.docker.internal>
2020-08-24 01:21:30 -0300

    Criacao de nova classe de teste

---
+++ src/test/java/br/com/mksistemas/fluxos/padrao/FluxoRNAPadraoTest.java
@@ -0,0 +1,19 @@
+package br.com.mksistemas.fluxos.padrao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FluxoRNAPadraoTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
