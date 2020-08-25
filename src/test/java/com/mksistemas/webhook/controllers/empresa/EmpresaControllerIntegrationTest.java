2b4060c072d89e4d1a9a2f756beff0ecc655a6c3
MkSistemas <MkSistemas@host.docker.internal>
2020-08-25 18:37:17 -0300

    Teste sobre uma acao de controlador

---
+++ src/test/java/com/mksistemas/webhook/controllers/empresa/EmpresaControllerIntegrationTest.java
@@ -0,0 +1,75 @@
+package com.mksistemas.webhook.controllers.empresa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mksistemas.webhook.MksistemaswebhookApplication;
+
+import br.com.mksistemas.base.negocio.MensagensDeResposta;
+import br.com.mksistemas.base.negocio.RespostaRequisicao;
+import br.com.mksistemas.rna.empresa.registrar.RegistrarEmpresaContexto;
+import br.com.mksistemas.rna.empresa.registrar.RegistrarEmpresaRequisicao;
+import br.com.mksistemas.rna.empresa.registrar.RegistrarEmpresaResposta;
+import br.com.mksistemas.rna.fluxos.padrao.IFluxoRNAPadrao;
+
+@SpringBootTest(classes = MksistemaswebhookApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+class EmpresaControllerIntegrationTest {
+
+	@Autowired
+	ObjectMapper jsonMapper;
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	IFluxoRNAPadrao<RegistrarEmpresaRequisicao, RegistrarEmpresaResposta, RegistrarEmpresaContexto> registrarEmpresa;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testRegistrarEmpresa() throws Exception {
+		RegistrarEmpresaCommand command = new RegistrarEmpresaCommand();
+		command.setId(UUID.randomUUID());
+		command.setNome("Empresa Teste");
+		command.setCnpj(46388366000180l);
+
+		var jsonData = jsonMapper.writeValueAsString(command);
+
+		RegistrarEmpresaResposta respostax = new RegistrarEmpresaResposta(
+				RespostaRequisicao.criar(MensagensDeResposta.ProcessamentoValido));
+
+		var result = mvc
+			.perform(post("/api/registrarempresa")
+			.content(jsonData)
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andReturn();
+		
+		jsonData = result.getResponse().getContentAsString();
+		var resposta = jsonMapper.readValue(jsonData, RegistrarEmpresaRespostaView.class);
+		assertEquals(MensagensDeResposta.ProcessamentoValido.getCodigo(), resposta.getResposta().getCodigo());
+	}
+
+}
+
+interface IRegistrarEmpresa
+		extends IFluxoRNAPadrao<RegistrarEmpresaRequisicao, RegistrarEmpresaResposta, RegistrarEmpresaContexto> {
+
+}
