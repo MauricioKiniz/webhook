38733a2bfee183fb1188d9778437aa0d22f2ac21
MkSistemas <MkSistemas@host.docker.internal>
2020-08-25 09:17:53 -0300

    remocao de documentos nao mais usados

--- src/main/java/br/com/mksistemas/rna/fluxos/padrao/ITratarExcecao.java
+++
@@ -1,8 +0,0 @@
-package br.com.mksistemas.rna.fluxos.padrao;
-
-import br.com.mksistemas.base.negocio.RespostaRequisicao;
-
-public interface ITratarExcecao<TContexto> {
-	public RespostaRequisicao executarNaRegraDeNegocio(TContexto contexto, Exception e);
-	public RespostaRequisicao executarNaPersistencia(TContexto contexto, Exception e);
-}
